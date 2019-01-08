8a3011188f374611eba59492185a8840301ed716
Marcos Lessa <mlessa@gmail.com>
2019-01-08 22:56:58 +0000

    Add Header

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import GeneralStyles from './styles/GeneralStyles';
+import Header from  './Header_Footer/Header';
 
 class Page extends Component {
   render() {
     return (
-      <div>
-        Hey
-      </div>
+      <GeneralStyles>
+        <Header />
+        <div className="page_container">
+          {this.props.children}
+        </div>
+        Footer
+      </GeneralStyles>
     );
   }
 }
