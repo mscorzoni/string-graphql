733bb29062462c9ebf6ad06e04064198e61ec476
Marcos Lessa <mlessa@gmail.com>
2019-01-14 14:30:16 +0000

    deploy brand wood and product types

---
+++ frontend/components/Shop/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Shop extends Component {
+
+  state = {
+    grid
+  }
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+
+export default Shop;
