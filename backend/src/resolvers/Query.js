3a5acaa83f8240695119138a814e3249c4ccf3e0
Marcos Lessa <mlessa@gmail.com>
2019-01-19 18:04:12 +0000

    fetch query brands

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,7 +1,9 @@
 const { forwardTo } = require('prisma-binding')
+
 const Query = {
   products: forwardTo('db'),
-  product: forwardTo('db')
+  product: forwardTo('db'),
+  brands: forwardTo('db'),
 };
 
 module.exports = Query;
--- frontend/components/Shop/index.js
+++ frontend/components/Shop/index.js
@@ -6,6 +6,8 @@ import faBars from '@fortawesome/fontawesome-free-solid/faBars';
 import faTh from '@fortawesome/fontawesome-free-solid/faTh';
 import LoadMoreCards from './LoadMoreCards';
 import FormatMoney from '../../lib/formatMoney';
+import PageTop from '../utils/PageTop';
+import CollapseCheckbox from '../utils/CollapseCheckbox';
 
 
 
@@ -26,11 +28,28 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ALL_BRANDS_QUERY = gql`
+  query ALL_BRANDS_QUERY {
+    brands{
+      id
+      name
+    }
+  }
+`;
+
 class Shop extends Component {
 
   state = {
-    grid: ''
+    grid: '',
+    filters: {
+      brand: [],
+      frets: [],
+      wood: [],
+      price: []
+    },
+    brands: ''
   }
+
 
   handleGrid= () => {
     this.setState({
@@ -44,29 +63,42 @@ class Shop extends Component {
           if(loading) return <p>Loading...</p>
           if(error) return <p>Error ...</p>
           return (
-            <div className="container">
-              <div className="shop_wrapper">
-                <div className="right">
-                  <div className="shop_options">
-                    <div className="shop_grids clear">
-                      <div
-                          className={`grid_btn ${this.state.grid ? '' : 'active' }`}
-                        >
-                          <FontAwesomeIcon icon={faTh}/>
-                        </div>
+            <div>
+              <PageTop  title="Browse Products" />
+              <div className="container">
+                <div className="shop_wrapper">
+                  <div className="left" > 
+                    <Query query={ALL_BRANDS_QUERY}>
+                      {({ data }) => (
+                      <CollapseCheckbox 
+                        title="Brands"
+                        list={data.brands}
+                        />
+                      )}
+                    </Query>
+                  </div>
+                  <div className="right"> 
+                    <div className="shop_options">
+                      <div className="shop_grids clear">
                         <div
-                            className={`grid_btn ${!this.state.grid ? '' :  'active'}`}
-                        >
-                          <FontAwesomeIcon icon={faBars} />
+                            className={`grid_btn ${this.state.grid ? '' : 'active' }`}
+                          >
+                            <FontAwesomeIcon icon={faTh}/>
+                          </div>
+                          <div
+                              className={`grid_btn ${!this.state.grid ? '' :  'active'}`}
+                          >
+                            <FontAwesomeIcon icon={faBars} />
+                          </div>
                         </div>
                       </div>
+                        
+                    <div className={{ clear: 'both' }}>
+                        <LoadMoreCards 
+                            grid={ this.state.grid }
+                            products={data.products}
+                        />
                     </div>
-                      
-                  <div className={{ clear: 'both' }}>
-                      <LoadMoreCards 
-                          grid={ this.state.grid }
-                          products={data.products}
-                      />
                   </div>
                 </div>
               </div>
